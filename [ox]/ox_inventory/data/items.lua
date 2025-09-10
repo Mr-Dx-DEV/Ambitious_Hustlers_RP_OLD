830227c3613fa26ab57321fba2e7b59fb1928e01
Tanvir Anjum Neon <taneon98@gmail.com>
2025-09-11 02:44:19 +0600

    Add new medical and law enforcement items
    Introduced several new items to the inventory, including medical supplies (medikit, medbag, tweezers, suture kit, ice pack, burn cream, defibrillator, sedative, morphine, percocet, vicodin, recovered bullet) and law enforcement tools (handcuffs, bobby pin, tracking bracelet). Also removed the old handcuffs entry and updated item properties such as weight, stackability, and close behavior.

--- [ox]/ox_inventory/data/items.lua
+++ [ox]/ox_inventory/data/items.lua
@@ -1,4 +1,144 @@
 return {
+
+    ['handcuffs'] = {
+        label = 'Hand Cuffs',
+        weight = 2,
+        stack = true,
+        close = true,
+    },
+
+    ['bobby_pin'] = {
+        label = 'Bobby Pin',
+        weight = 2,
+        stack = true,
+        close = true,
+    },
+
+    ['tracking_bracelet '] = {
+        label = 'Tracking Bracelet',
+        weight = 2,
+        stack = true,
+        close = true,
+    },
+
+
+
+    ['medikit'] = {   -- Make sure not already a medikit
+        label = 'Medikit',
+        weight = 165,
+        stack = true,
+        close = true,
+    },
+    ['medbag'] = {
+        label = 'Medical Bag',
+        weight = 165,
+        stack = false,
+        close = true,
+    },
+
+    ['tweezers'] = {
+        label = 'Tweezers',
+        weight = 2,
+        stack = true,
+        close = true,
+    },
+
+    ['suturekit'] = {
+        label = 'Suture Kit',
+        weight = 15,
+        stack = true,
+        close = true,
+    },
+
+    ['icepack'] = {
+        label = 'Ice Pack',
+        weight = 29,
+        stack = true,
+        close = true,
+    },
+
+    ['burncream'] = {
+        label = 'Burn Cream',
+        weight = 19,
+        stack = true,
+        close = true,
+    },
+
+    ['defib'] = {
+        label = 'Defibrillator',
+        weight = 225,
+        stack = false,
+        close = true,
+    },
+
+    ['sedative'] = {
+        label = 'Sedative',
+        weight = 15,
+        stack = true,
+        close = true,
+    },
+
+    ['morphine30'] = {
+        label = 'Morphine 30MG',
+        weight = 2,
+        stack = true,
+        close = true,
+    },
+
+    ['morphine15'] = {
+        label = 'Morphine 15MG',
+        weight = 2,
+        stack = true,
+        close = true,
+    },
+
+    ['perc30'] = {
+        label = 'Percocet 30MG',
+        weight = 2,
+        stack = true,
+        close = true,
+    },
+
+    ['perc10'] = {
+        label = 'Percocet 10MG',
+        weight = 2,
+        stack = true,
+        close = true,
+    },
+
+    ['perc5'] = {
+        label = 'Percocet 5MG',
+        weight = 2,
+        stack = true,
+        close = true,
+    },
+
+    ['vic10'] = {
+        label = 'Vicodin 10MG',
+        weight = 2,
+        stack = true,
+        close = true,
+    },
+
+    ['vic5'] = {
+        label = 'Vicodin 5MG',
+        weight = 2,
+        stack = true,
+        close = true,
+    },
+
+    ['recoveredbullet'] = {
+        label = 'Recovered Bullet',
+        weight = 1,
+        stack = true,
+        close = false,
+    },
+
+
+
+
+
+
     ['testburger'] = {
         label = 'Test Burger',
         weight = 220,
@@ -136,7 +276,7 @@ return {
         weight = 19,
         stack = false,
     },
-    
+
 
     ['mustard'] = {
         label = 'Mustard',
@@ -624,10 +764,5 @@ return {
     ['harness'] = {
         label = 'Harness',
         weight = 200,
-    },
-
-    ['handcuffs'] = {
-        label = 'Handcuffs',
-        weight = 200,
-    },
+    }
 }
