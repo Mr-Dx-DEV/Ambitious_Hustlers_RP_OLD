2253a4f0f70c13e954976b8755eddfed59cae144
Tanvir Anjum Neon <taneon98@gmail.com>
2025-09-12 01:29:43 +0600

    Comment out evidence locations in evidence.lua
    The evidence location entries have been commented out, effectively disabling them. This may be for testing, debugging, or temporarily removing evidence zones from the configuration.

--- [ox]/ox_inventory/data/evidence.lua
+++ [ox]/ox_inventory/data/evidence.lua
@@ -1,27 +1,27 @@
 return {
-	{
-		coords = vec3(463.78, -972.91, 26.87),
-		target = { -- qtarget support
-			name = 'mrpd_evidence', -- name of zone must be uniuqe
-			loc = vec3(463.78, -972.91, 26.87),
-			length = 1.5,
-			width = 3.2,
-			heading = 0,
-			minZ = 29.09,
-			maxZ = 31.89
-		}
-	},
-	{
-		coords = vec3(2801.98, 4748.65, 43.73),
-		target = { -- qtarget support
-			name = 'sahp1_evidence', -- name of zone must be uniuqe
-			loc = vec3(2801.98, 4748.65, 43.73),
-			length = 1.7,
-			width = 3.2,
-			heading = 0,
-			minZ = 42.09,
-			maxZ = 44.89
-		}
-	}
+	-- {
+	-- 	coords = vec3(463.78, -972.91, 26.87),
+	-- 	target = { -- qtarget support
+	-- 		name = 'mrpd_evidence', -- name of zone must be uniuqe
+	-- 		loc = vec3(463.78, -972.91, 26.87),
+	-- 		length = 1.5,
+	-- 		width = 3.2,
+	-- 		heading = 0,
+	-- 		minZ = 29.09,
+	-- 		maxZ = 31.89
+	-- 	}
+	-- },
+	-- {
+	-- 	coords = vec3(2801.98, 4748.65, 43.73),
+	-- 	target = { -- qtarget support
+	-- 		name = 'sahp1_evidence', -- name of zone must be uniuqe
+	-- 		loc = vec3(2801.98, 4748.65, 43.73),
+	-- 		length = 1.7,
+	-- 		width = 3.2,
+	-- 		heading = 0,
+	-- 		minZ = 42.09,
+	-- 		maxZ = 44.89
+	-- 	}
+	-- }
 	
 }
