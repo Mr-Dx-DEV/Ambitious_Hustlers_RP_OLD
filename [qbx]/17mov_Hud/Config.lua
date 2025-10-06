1872f88b5668127540cd0d131c42c4fef7ec9294
SBDemon <hellrasier95@gmail.com>
2025-10-06 09:07:16 -0400

    sTASH

--- [qbx]/17mov_Hud/Config.lua
+++ [qbx]/17mov_Hud/Config.lua
@@ -36,7 +36,7 @@ Config.ShowRadarIfHidden = true -- If radar is hidden and you set Config.BigRada
 
 -- Stress system
 Config.EnableStress = true           -- Use build-in stress system
-Config.AddStressWhileShooting = true -- Should add stress while shooting?
+Config.AddStressWhileShooting = false -- Should add stress while shooting?
 Config.StressingSpeed = 100          -- Speed above which you start to get stressed
 Config.MinimumStressForEffects = 50  -- Minimum Stress amount to start effects
 Config.WhtielistedWeapons = {        -- Here you can add some weapon models that "bypassing" stress system (not adding stress while shooting)
