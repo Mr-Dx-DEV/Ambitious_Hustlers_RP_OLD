64798e3abb00b7a5df002fac3023d8581d5d1e88
SBDemon <hellrasier95@gmail.com>
2025-10-27 21:45:19 -0400

    detecting

--- [standalone]/lation_detecting/fxmanifest.lua
+++ [standalone]/lation_detecting/fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 name 'lation_detecting'
 author 'iamlation'
 description 'An advanced metal detecting resource for FiveM'
-version '4.0.6'
+version '2.0.7'
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
