867c4dd39933d299867c843c6ae1206fda2ddb88
SBDemon <hellrasier95@gmail.com>
2025-10-28 01:19:44 -0400

    pro bar

--- [standalone]/jim-consumables/config.lua
+++ [standalone]/jim-consumables/config.lua
@@ -10,10 +10,10 @@ Config = {
 
 	},
 	Main = {
-		UseProgbar = false,		-- Recommended to disable this, progress bars stop ALL anims when complete.
+		UseProgbar = true,		-- Recommended to disable this, progress bars stop ALL anims when complete.
 								-- This is intended to create a progressbar to show consuming but it was usually forces animations to end weirdly
 	},
 	Crafting = {
-		showItemBox = true		-- Display "use" item boxes on some inventories that need to be told to
+		showItemBox = false		-- Display "use" item boxes on some inventories that need to be told to
 	}
 }
