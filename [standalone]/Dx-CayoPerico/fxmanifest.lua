c83a36029f3c94f1a439ce1a5aa5fe88a72090d9
SBDemon <hellrasier95@gmail.com>
2025-10-28 01:19:22 -0400

    cayo

---
+++ [standalone]/Dx-CayoPerico/fxmanifest.lua
@@ -0,0 +1,12 @@
+
+fx_version 'bodacious'
+game 'gta5'
+this_is_a_map 'yes'
+client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/EntityZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',    
+    'load.lua'
+}
